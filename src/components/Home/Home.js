118aafface18ce8a0f2a72dc567755d3a3ace281
Austin Papritz <austin@papritz.dev>
2023-01-11 15:26:29 -0800

    added wireframe to readme

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Preview from '../Preview/Preview.js';
+import Editor from '../Editor/Editor.js';
+
+export default function Home() {
+  return (
+    <main>
+      <Preview />
+      <Editor />
+    </main>
+  );
+}
