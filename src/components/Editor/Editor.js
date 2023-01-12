f3a1cde507b019d4a994338ec053d9dd06f69d0d
Austin Papritz <austin@papritz.dev>
2023-01-11 16:01:32 -0800

    select and options for head, body, and bottom; Catchphrase and Scoreboard improted to Editor.js

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,5 +1,34 @@
 import React from 'react';
 
+import Catchphrase from '../Catchphrase/Catchphrase';
+import Scoreboard from '../Scoreboard/Scoreboard';
+
 export default function Editor() {
-  return <div>Editor</div>;
+  return (
+    <div className="editor-container">
+      <select className="head-select">
+        <option value="bird">Bird</option>
+        <option value="dog">Dog</option>
+        <option value="duck">Duck</option>
+        <option value="horse">Horse</option>
+      </select>
+      <label>Select a head</label>
+      <select className="body-select">
+        <option value="blue">Blue</option>
+        <option value="dress">Dress</option>
+        <option value="pink">Pink</option>
+        <option value="red">Red</option>
+      </select>
+      <label>Select a body</label>
+      <select className="bottom-select">
+        <option value="blue">Blue</option>
+        <option value="dog">Dog</option>
+        <option value="legs">Legs</option>
+        <option value="white">White</option>
+      </select>
+      <label>Select a bottom</label>
+      <Catchphrase />
+      <Scoreboard />
+    </div>
+  );
 }
