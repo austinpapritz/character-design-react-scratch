c88a06b9f9c529b537682c8eb5009fc059539cc2
Austin Papritz <austin@papritz.dev>
2023-01-11 16:43:19 -0800

    added background to App div

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-
+import Background from './bg.jpeg';
 import Home from './components/Home/Home.js';
 
-import Header from '../src/components/Layout/Header/Header';
-import Footer from '../src/components/Layout/Footer/Footer';
+import Header from '../src/components/Layout/Header/Header.js';
+import Footer from '../src/components/Layout/Footer/Footer.js';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundImage: `url('${Background}')` }}>
       <Header />
       <Home />
       <Footer />
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,12 @@
 import React from 'react';
+import './Home.css';
 
 import Preview from '../Preview/Preview.js';
 import Editor from '../Editor/Editor.js';
 
-import Background from './bg.jpeg';
-
 export default function Home() {
   return (
-    <main style={{ backgroundImage: `url('${Background}')` }}>
+    <main>
       <Preview />
       <Editor />
     </main>
