aced0fb1c475bc130c5d637ec8e06b33a79f9596
Austin Papritz <austin@papritz.dev>
2023-01-11 16:16:36 -0800

    added component folders for Header and Footer

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import './App.css';
 
 import Home from './components/Home/Home.js';
 
+import Header from '../src/components/Layout/Header/Header';
+import Footer from '../src/components/Layout/Footer/Footer';
+
 function App() {
   return (
     <div className="App">
+      <Header />
       <Home />
+      <Footer />
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,9 +3,11 @@ import React from 'react';
 import Preview from '../Preview/Preview.js';
 import Editor from '../Editor/Editor.js';
 
+import Background from './bg.jpeg';
+
 export default function Home() {
   return (
-    <main>
+    <main style={{ backgroundImage: `url('${Background}')` }}>
       <Preview />
       <Editor />
     </main>
