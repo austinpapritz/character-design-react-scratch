fee5ab56f1fecb6ef98483fb90881df70d95d918
Austin Papritz <austin@papritz.dev>
2023-01-12 09:30:48 -0800

    background image for App now working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-// import Background from '/assets/bg.jpeg';
 import Home from './components/Home/Home.js';
 
 import Header from '../src/components/Layout/Header/Header.js';
@@ -7,7 +6,6 @@ import Footer from '../src/components/Layout/Footer/Footer.js';
 
 function App() {
   return (
-    // <div className="App" style={{ backgroundImage: `url('${Background}')` }}>
     <div className="App">
       <Header />
       <Home />
