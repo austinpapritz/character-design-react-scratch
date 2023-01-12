ffe854efcf9edbd6a4b2dc29b7318cc9c6226e54
Austin Papritz <austin@papritz.dev>
2023-01-12 09:53:00 -0800

    a little css on editor selects

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -19,29 +19,31 @@ export default function Editor({ setHead, setBody, setBottom }) {
 
   return (
     <div className="editor-container">
-      <select className="head-select" onChange={handleHead}>
-        <option value="bird">Bird</option>
-        <option value="dog">Dog</option>
-        <option value="duck">Duck</option>
-        <option value="horse">Horse</option>
-      </select>
-      <label>Select a head</label>
-      <select className="body-select" onChange={handleBody}>
-        <option value="blue">Blue</option>
-        <option value="dress">Dress</option>
-        <option value="pink">Pink</option>
-        <option value="red">Red</option>
-      </select>
-      <label>Select a body</label>
-      <select className="bottom-select" onChange={handleBottom}>
-        <option value="blue">Blue</option>
-        <option value="dog">Dog</option>
-        <option value="legs">Legs</option>
-        <option value="white">White</option>
-      </select>
-      <label>Select a bottom</label>
-      <Catchphrase />
-      <Scoreboard />
+      <div className="editor-content">
+        <select className="head-select" onChange={handleHead}>
+          <option value="bird">Bird</option>
+          <option value="dog">Dog</option>
+          <option value="duck">Duck</option>
+          <option value="horse">Horse</option>
+        </select>
+        <label>Select a head</label>
+        <select className="body-select" onChange={handleBody}>
+          <option value="blue">Blue</option>
+          <option value="dress">Dress</option>
+          <option value="pink">Pink</option>
+          <option value="red">Red</option>
+        </select>
+        <label>Select a body</label>
+        <select className="bottom-select" onChange={handleBottom}>
+          <option value="blue">Blue</option>
+          <option value="dog">Dog</option>
+          <option value="leg">Leg</option>
+          <option value="white">White</option>
+        </select>
+        <label>Select a bottom</label>
+        <Catchphrase />
+        <Scoreboard />
+      </div>
     </div>
   );
 }
