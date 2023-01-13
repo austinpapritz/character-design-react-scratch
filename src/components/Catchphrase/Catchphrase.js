e14dfe92bd333600aa59d4c7bb712098c0ef9f0f
Austin Papritz <austin@papritz.dev>
2023-01-12 17:57:27 -0800

    added very basic catchphrase component and css

--- src/components/Catchphrase/Catchphrase.js
+++ src/components/Catchphrase/Catchphrase.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import './Catchphrase.css';
 
 export default function Catchphrase() {
-  return <div>Catchphrase</div>;
+  return (
+    <div className="catchphrase-container">
+      <input></input>
+      <button>Submit</button>
+      <label>Add a catchphrase</label>
+    </div>
+  );
 }
